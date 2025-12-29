4acdc3914ef43bfe4bb5999be4245a2bf86318fd
Robert Yan <mstr.raphael@gmail.com>
2025-12-29 21:17:23 +0800

    style: update comment

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 . .venv/bin/activate
-# DEV=1 to enable DEV mode, which will generate random keys for testing
+# DEV=1 to enable DEV mode, which will generate random keys for testing, since
+# KMS is not available in non-TEE test environment.
 PYTHONPATH=src DEV=1 python -m pytest tests/ -v "$@"
