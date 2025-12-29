40e9ea029bc7ab041984a9b9134b0d50dcad72e6
Robert Yan <46699230+think-in-universe@users.noreply.github.com>
2025-12-29 21:17:47 +0800

    feat: derive signing key from kms (#10)

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 . .venv/bin/activate
-PYTHONPATH=src python -m pytest tests/ -v "$@"
+# DEV=1 to enable DEV mode, which will generate random keys for testing, since
+# KMS is not available in non-TEE test environment.
+PYTHONPATH=src DEV=1 python -m pytest tests/ -v "$@"
